7e003ee511ad62a15a83c2be79b73b61d6396b3a
Carlo Cardella <carloc@microsoft.com>
2020-09-03 22:02:11 -0700

    - remove duplicate lines and optionally open the result in a new text editor

--- src/modules/filterText.ts
+++ src/modules/filterText.ts
@@ -1,29 +1,63 @@
-import { getDocumentText, getActiveEditor, selectAllText } from './helpers';
-import * as os from 'os';
+import { getDocumentText, getActiveEditor, selectAllText, getDocumentTextOrSelection, createNewEditor, sleep, getActiveSelection, getSelectionOrFullDocument } from './helpers';
+import * as os from "os";
+import { window } from "vscode";
+import * as vscode from 'vscode';
 
 export async function removeEmptyLines(redundandOnly: boolean) {
     let text = getDocumentText();
-    if (!text) { return; }
+    if (!text) {
+        return;
+    }
 
     let o = os;
     let r;
     let rr: string;
     // /^\n{2,}/gm ==> two or more empty lines
     // /^\n+/gm    ==> any empty line
-    redundandOnly ? r = /^(\n{2,}|^(\r\n){2,})/gm : r = /^(\n+|\r\n+)/gm;
+    redundandOnly ? (r = /^(\n{2,}|^(\r\n){2,})/gm) : (r = /^(\n+|\r\n+)/gm);
     // replace multiple empty lines with a single one, or with nothing
-    redundandOnly ? rr = o.EOL : rr = "";
+    redundandOnly ? (rr = o.EOL) : (rr = "");
 
     const newText = text.replace(r, rr!);
 
     let editor = getActiveEditor();
-    if (!editor) { return; }
+    if (!editor) {
+        return;
+    }
 
     selectAllText().then(() => {
         let selection = editor?.selection;
-        if (!selection) { return; }
-        editor?.edit(editBuilder => {
+        if (!selection) {
+            return;
+        }
+        editor?.edit((editBuilder) => {
             editBuilder.replace(selection!, newText);
         });
+    });
+}
+
+export async function removeDuplicateLines(resultInNewEditor: boolean) {
+    let text = getDocumentTextOrSelection();
+    const o = os;
+    let lines = text?.split(os.EOL);
+    if (!lines) { return; }
+
+    for (const line of lines) {
+        while (lines.indexOf(line) !== lines.lastIndexOf(line)) {
+            lines.splice(lines.lastIndexOf(line), 1);
+        }
+    }
+    const newText = lines.join(o.EOL).replace(o.EOL, "");
+
+    if (resultInNewEditor) {
+        createNewEditor(newText);
+        return;
+    }
+
+    const editor = getActiveEditor();
+    let selection = getSelectionOrFullDocument(editor!);
+
+    editor!.edit(editBuilder => {
+        editBuilder.replace(selection!, newText);
     });
 }
--- src/test/suite/filterText.test.ts
+++ src/test/suite/filterText.test.ts
@@ -1,7 +1,9 @@
 import * as assert from 'assert';
 import { before, after, afterEach, describe } from 'mocha';
-import { closeTextEditor, sleep, getDocumentText, createNewEditor } from '../../modules/helpers';
-import { removeEmptyLines } from '../../modules/filterText';
+import { closeTextEditor, sleep, getDocumentText, createNewEditor, selectAllText } from '../../modules/helpers';
+import { removeDuplicateLines, removeEmptyLines } from '../../modules/filterText';
+import * as os from 'os';
+import { truncateSync } from 'fs';
 
 
 suite("filterText", () => {
@@ -16,9 +18,10 @@ suite("filterText", () => {
     });
 
     describe("Remove all empty lines", async () => {
-        const testEditorText = "Fehfomda pemup mihjeb\n\nuvonono nelvojpo wokragsi geligab\n\n\npokacan repafme racje ut alhacov\n\nHireme gahze\n\n\n\n\npi zo iro becago vekabo\nluihait abe zukuv gof tususho\n\n\n\n";
-        const testEditorTextRedundantExpected = "Fehfomda pemup mihjeb\n\nuvonono nelvojpo wokragsi geligab\n\npokacan repafme racje ut alhacov\n\nHireme gahze\n\npi zo iro becago vekabo\nluihait abe zukuv gof tususho\n\n";
-        const testEditorTextAllExpected = "Fehfomda pemup mihjeb\nuvonono nelvojpo wokragsi geligab\npokacan repafme racje ut alhacov\nHireme gahze\npi zo iro becago vekabo\nluihait abe zukuv gof tususho\n";
+        const eol = os.EOL;
+        const testEditorText = `Fehfomda pemup mihjeb${eol}${eol}uvonono nelvojpo wokragsi geligab${eol}${eol}${eol}pokacan repafme racje ut alhacov${eol}${eol}Hireme gahze${eol}${eol}${eol}${eol}${eol}pi zo iro becago vekabo${eol}luihait abe zukuv gof tususho${eol}${eol}${eol}${eol}`;
+        const testEditorTextRedundantExpected = `Fehfomda pemup mihjeb${eol}${eol}uvonono nelvojpo wokragsi geligab${eol}${eol}pokacan repafme racje ut alhacov${eol}${eol}Hireme gahze${eol}${eol}pi zo iro becago vekabo${eol}luihait abe zukuv gof tususho${eol}${eol}`;
+        const testEditorTextAllExpected = `Fehfomda pemup mihjeb${eol}uvonono nelvojpo wokragsi geligab${eol}pokacan repafme racje ut alhacov${eol}Hireme gahze${eol}pi zo iro becago vekabo${eol}luihait abe zukuv gof tususho${eol}`;
 
         let tests = [
             { docText: testEditorText, redundantOnly: true, expected: testEditorTextRedundantExpected },
@@ -31,6 +34,37 @@ suite("filterText", () => {
             test("Filter " + itRedundant + " empty lines", async () => {
                 await createNewEditor(testEditorText);
                 await removeEmptyLines(t.redundantOnly);
+                await sleep(500);
+
+                let text = getDocumentText();
+                assert.deepStrictEqual(text, t.expected);
+            });
+        });
+    });
+
+    describe("Remove duplicate lines", async () => {
+        const eol = os.EOL;
+        const testEditorDuplicateLines = `Gowul ibbohu tafeid fokecdif lab adazujob${eol}meaf dekase sij of wehi nefowumu wizabeti${eol}${eol}Gowul ibbohu tafeid fokecdif lab adazujob${eol}Gowul ibbohu tafeid fokecdif lab adazujob${eol}${eol}${eol}palte${eol}${eol}${eol}meaf dekase sij of wehi nefowumu wizabeti${eol}meaf dekase sij of wehi nefowumu wizabeti${eol}meaf dekase sij of wehi nefowumu wizabeti${eol}${eol}${eol}Nefzuh toehe jiubvid tic didukod ehe ji ${eol}ana mur tiofapel sudvivot hub wurgo jifhi jumkehfot ${eol}palte${eol}${eol}ana mur tiofapel sudvivot hub wurgo jifhi jumkehfot ${eol}`;
+        const testEditorDuplicateLinesRemoved = `Gowul ibbohu tafeid fokecdif lab adazujob${eol}meaf dekase sij of wehi nefowumu wizabeti${eol}palte${eol}Nefzuh toehe jiubvid tic didukod ehe ji${eol}ana mur tiofapel sudvivot hub wurgo jifhi jumkehfot${eol}`;
+
+        const tests = [
+            { docText: testEditorDuplicateLines, selection: true, openInNewEditor: false, expected: testEditorDuplicateLinesRemoved },
+            { docText: testEditorDuplicateLines, selection: false, openInNewEditor: false, expected: testEditorDuplicateLinesRemoved },
+            { docText: testEditorDuplicateLines, selection: true, openInNewEditor: true, expected: testEditorDuplicateLinesRemoved },
+            { docText: testEditorDuplicateLines, selection: false, openInNewEditor: true, expected: testEditorDuplicateLinesRemoved },
+        ];
+
+        tests.forEach(function (t) {
+            let testTitle: string;
+            if (t.selection && !t.openInNewEditor) { testTitle = "Remove duplicate lines and update selection"; }
+            else if (!t.selection && !t.openInNewEditor) { testTitle = "Remove duplicate lines from then document"; }
+            else if (t.selection && t.openInNewEditor) { testTitle = "Remove duplciate lines, open result in new editor"; }
+            else if (!t.selection && t.openInNewEditor) { testTitle = "Remove duplicate lines from the document, open resul in new editor"; }
+
+            test(testTitle!, async () => {
+                await createNewEditor(t.docText);
+                if (t.selection) { await selectAllText(); }
+                await removeDuplicateLines(t.openInNewEditor);
                 await sleep(500);
 
                 let text = getDocumentText();
