89acc82ab182fc09be59b847e5b31ca3f5519ea7
Carlo Cardella <Carloc@microsoft.com>
2021-07-11 09:19:34 -0700

    update tests

--- src/test/suite/splitText.test.ts
+++ src/test/suite/splitText.test.ts
@@ -16,11 +16,11 @@ suite("splitText", () => {
     });
 
     describe("Split Text", () => {
-        const textToSplit = `Igceb rud ezwudel zu guc nuh, beoputac zidoti towposi alvez jomojsot, luhminka ve nabomo su wipwir.${EOL}Vo,zoenpi,zassagon,ipauv,kojuf,meronewis,degveam,zopev,anowoofi,ewol,disin,mo,la,olobozad,kafwa.,Vefoegu,vazuaw,jomo,cel,bim,zam,upmele,dinga,zulkuc,div,ca,kuratim,joim,avi,liut,bife,eranijoh.,Rezom,kuwher,ricaslo,je,rojhofil,cugi,cohjarjo,ev,wimecarar,ov,bi,egi,ucure.${EOL}Kelgi etfaelu vurozvu suduta kilaw tilespeg wojma cirgoja dezpa ot hajuidu, iru tukihuvap dupsatig dam usto wuasi.`;
+        const textToSplit = `Aute ex anim dolore sunt pariatur culpa sit ut ut ex incididunt mollit dolore.${EOL}Irure, enim, culpa, reprehenderit, aliqua, laboris, irure, sint, proident, labore, quis, laboris, quis, quis.${EOL}Aute proident mollit eiusmod fugiat.`;
 
-        const textSplitSecondLine = `Igceb rud ezwudel zu guc nuh, beoputac zidoti towposi alvez jomojsot, luhminka ve nabomo su wipwir.${EOL}Vo${EOL}zoenpi${EOL}zassagon${EOL}ipauv${EOL}kojuf${EOL}meronewis${EOL}degveam${EOL}zopev${EOL}anowoofi${EOL}ewol${EOL}disin${EOL}mo${EOL}la${EOL}olobozad${EOL}kafwa.${EOL}Vefoegu${EOL}vazuaw${EOL}jomo${EOL}cel${EOL}bim${EOL}zam${EOL}upmele${EOL}dinga${EOL}zulkuc${EOL}div${EOL}ca${EOL}kuratim${EOL}joim${EOL}avi${EOL}liut${EOL}bife${EOL}eranijoh.${EOL}Rezom${EOL}kuwher${EOL}ricaslo${EOL}je${EOL}rojhofil${EOL}cugi${EOL}cohjarjo${EOL}ev${EOL}wimecarar${EOL}ov${EOL}bi${EOL}egi${EOL}ucure.${EOL}Kelgi etfaelu vurozvu suduta kilaw tilespeg wojma cirgoja dezpa ot hajuidu, iru tukihuvap dupsatig dam usto wuasi.`;
+        const textSplitSecondLine = `Aute ex anim dolore sunt pariatur culpa sit ut ut ex incididunt mollit dolore.${EOL}Irure${EOL} enim${EOL} culpa${EOL} reprehenderit${EOL} aliqua${EOL} laboris${EOL} irure${EOL} sint${EOL} proident${EOL} labore${EOL} quis${EOL} laboris${EOL} quis${EOL} quis.${EOL}Aute proident mollit eiusmod fugiat.`;
 
-        const textSplitFirstLineNewEditor = `Igceb rud ezwudel zu guc nuh${EOL} beoputac zidoti towposi alvez jomojsot${EOL} luhminka ve nabomo su wipwir.`;
+        const textSplitFirstLineNewEditor = `Aute${EOL}ex${EOL}anim${EOL}dolore${EOL}sunt${EOL}pariatur${EOL}culpa${EOL}sit${EOL}ut${EOL}ut${EOL}ex${EOL}incididunt${EOL}mollit${EOL}dolore.${EOL}Irure, enim, culpa, reprehenderit, aliqua, laboris, irure, sint, proident, labore, quis, laboris, quis, quis.${EOL}Aute proident mollit eiusmod fugiat.`;
 
         test("Split the second line in a text of three", async () => {
             await createNewEditor(textToSplit);
@@ -40,7 +40,7 @@ suite("splitText", () => {
             const editor = getActiveEditor();
             // editor!.selections = [new Selection(0, 0, 0, editor?.document.lineAt(1).text.length!)];
             editor!.selections = [new Selection(0, 0, 0, 99)];
-            await splitTextInternal(",", true);
+            await splitTextInternal(" ", true);
             await sleep(500);
 
             await selectAllText();
