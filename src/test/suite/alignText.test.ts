f1a9a382b9bbb68636356c89e209714ad9e8bfda
Carlo Cardella <carloc@microsoft.com>
2021-11-05 18:45:18 -0700

    add tests for "Align" commands

--- src/test/suite/alignText.test.ts
+++ src/test/suite/alignText.test.ts
@@ -1,36 +1,53 @@
-import * as assert from 'assert';
-import { after, before, describe } from 'mocha';
-import { EOL } from 'os';
+import * as assert from "assert";
+import { after, before, describe } from "mocha";
+import { EOL } from "os";
 import { alignToSeparator } from '../../modules/alignText';
 import { closeTextEditor, sleep, createNewEditor, selectAllText, getDocumentTextOrSelection } from '../../modules/helpers';
 
-
-suite('alignText', () => {
+suite("alignText", () => {
     before(() => {
-        console.log('Starting alignText tests');
+        console.log("Starting alignText tests");
     });
     after(async () => {
         await sleep(500);
         await closeTextEditor(true);
-        console.log('All alignText tests done');
+        console.log("All alignText tests done");
     });
 
     describe("Align text to separator", () => {
-        let newText = `Hew lihu riznen fo wour omju, ojhomo bewe mo kul, taten bozi fe gonzabhu acoitotez, rizzapev abwizfa ca.${EOL}Hif marfi busaume, zobbihi, javelvaf kukapgo bopuwi ajo gamdi anosve, muiwizon lago jape gahihgod podehdab.${EOL}Tec piawa ma mimbim jotenejos bidnar, divtovo wufe cov sah fozgaw, niobete azonar ujajono akuek wimgob.`;
-        let expectedSpace = `Hew  lihu   riznen    fo        wour       omju,    ojhomo   bewe  mo     kul,     taten     bozi     fe      gonzabhu  acoitotez,  rizzapev  abwizfa  ca.  ${EOL}Hif  marfi  busaume,  zobbihi,  javelvaf   kukapgo  bopuwi   ajo   gamdi  anosve,  muiwizon  lago     jape    gahihgod  podehdab.   ${EOL}Tec  piawa  ma        mimbim    jotenejos  bidnar,  divtovo  wufe  cov    sah      fozgaw,   niobete  azonar  ujajono   akuek       wimgob.   `;
-        let expectedComma = `Hew lihu riznen fo wour omju,          ojhomo bewe mo kul,           taten bozi fe gonzabhu acoitotez,          rizzapev abwizfa ca.                   ${EOL}Hif marfi busaume,                     zobbihi,                      javelvaf kukapgo bopuwi ajo gamdi anosve,  muiwizon lago jape gahihgod podehdab.  ${EOL}Tec piawa ma mimbim jotenejos bidnar,  divtovo wufe cov sah fozgaw,  niobete azonar ujajono akuek wimgob.,     ${EOL}`;
+        let newText = `asdfasdfasd,fadfasdfasdfasdf,adsfadf,asdf,asdfa,df${EOL}asdasdfa,sdfasdfa,sdf,adsf,asd,fasdfasdfadsf,${EOL}asd,sdfasdfa,dsf,asdfa,sdf,as,df,a,df,adfadfa`;
+        let expectedComma = `asdfasdfasd, fadfasdfasdfasdf, adsfadf, asdf,  asdfa, df,            ,,,,${EOL}asdasdfa,    sdfasdfa,         sdf,     adsf,  asd,   fasdfasdfadsf, ,,,,${EOL}asd,         sdfasdfa,         dsf,     asdfa, sdf,   as,            df, a, df, adfadfa ${EOL}`;
 
+        let tests = [{ newText: newText, separator: ",", expected: expectedComma }];
 
-        let tests = [
-            { newText: newText, separator: ",", expected: expectedComma },
-            // { newText: newText, delimiter: " ", expected: expectedSpace }
-        ];
-
-        tests.forEach(t => {
+        tests.forEach((t) => {
             test(`Align text to separator '${t.separator}'`, async () => {
                 await createNewEditor(t.newText);
                 await selectAllText();
                 await alignToSeparator(t.separator);
+                await sleep(500);
+
+                const text = getDocumentTextOrSelection();
+                assert.deepStrictEqual(text, t.expected);
+            });
+        });
+    });
+
+    describe("Align text as table", () => {
+        let newText = `asdfasdfasd,fadfasdfasdfasdf,adsfadf,asdf,asdfa,df${EOL}asdasdfa,sdfasdfa,sdf,adsf,asd,fasdfasdfadsf,${EOL}asd,sdfasdfa,dsf,asdfa,sdf,as,df,a,df,adfadfa`;
+        let expectedTable = `| asdfasdfasd | fadfasdfasdfasdf | adsfadf | asdf  | asdfa | df            |    |   |    |         |${EOL}| asdasdfa    | sdfasdfa         | sdf     | adsf  | asd   | fasdfasdfadsf |    |   |    |         |${EOL}| asd         | sdfasdfa         | dsf     | asdfa | sdf   | as            | df | a | df | adfadfa |${EOL}`;
+        let expectedTableWithHeaders = `| asdfasdfasd | fadfasdfasdfasdf | adsfadf | asdf  | asdfa | df            |    |   |    |         |${EOL}|-------------|------------------|---------|-------|-------|---------------|----|---|----|---------|${EOL}| asdasdfa    | sdfasdfa         | sdf     | adsf  | asd   | fasdfasdfadsf |    |   |    |         |${EOL}| asd         | sdfasdfa         | dsf     | asdfa | sdf   | as            | df | a | df | adfadfa |${EOL}`;
+
+        let tests = [
+            { newText: newText, separator: ",", withHeaders: false, expected: expectedTable },
+            { newText: newText, separator: ",", withHeaders: true, expected: expectedTableWithHeaders },
+        ];
+
+        tests.forEach((t) => {
+            test(`Align text as table ${t.withHeaders ? "with headers" : ""}`, async () => {
+                await createNewEditor(t.newText);
+                await selectAllText();
+                await alignToSeparator(t.separator, true, t.withHeaders);
                 await sleep(500);
 
                 const text = getDocumentTextOrSelection();
