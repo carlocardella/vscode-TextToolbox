b051cea0e50211504c0ec03f638564e357013d3b
Carlo Cardella <carloc@microsoft.com>
2025-09-09 10:42:45 -0700

    Clean up unnecessary debug and development test files
    - Remove test-json*.js files (JSON debugging scripts)
    - Remove quick-test.js (temporary module testing script)
    - Keep run-tests.js (essential test runner)
    - Update .gitignore to prevent similar debug files from being committed

--- quick-test.js
+++
@@ -1,4 +0,0 @@
-// Quick test to verify sortText fixes
-const sortText = require('./out/modules/sortText');
-console.log('Testing sortText module loaded successfully');
-console.log('sortDirection enum:', sortText.sortDirection);
