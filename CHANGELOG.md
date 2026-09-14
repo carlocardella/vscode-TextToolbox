ed671c750464f7ae582c4d0a3611f634cfcb8dd4
Carlo Cardella <carloc@microsoft.com>
2026-09-13 18:13:57 -0700

    chore: update dependencies and adjust webpack config
    - Bump @typescript-eslint/eslint-plugin and parser to version 8.69.0
    - Upgrade @vscode/vsce to version 3.9.2
    - Update mocha to version 12.0.0
    - Upgrade nyc to version 18.0.0
    - Remove crypto-browserify dependency in webpack config, setting crypto to false

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,12 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/) 
 
 ## Unreleased
 
+## [2.30.0] - 2026-09-13
+
+### Fixed
+
+* Update NPM packages
+
 ## [2.29.0] - 2025-09-30
 
 ### Added
--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ const webConfig = /** @type WebpackConfig */ {
             // Webpack 5 no longer polyfills Node.js core modules automatically.
             assert: require.resolve("assert"),
             buffer: require.resolve("buffer"),
-            crypto: require.resolve("crypto-browserify"),
+            crypto: false,
             os: require.resolve("os-browserify/browser"),
             path: require.resolve("path-browserify"),
             process: require.resolve("process/browser"),
